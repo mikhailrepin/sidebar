c129cccb637852d3a0d906f00bd18a31ce0d3eaf
Mikhail Repin <migel72333@gmail.com>
2025-05-29 11:59:05 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  template: `
+    <main class="flex flex-col h-screen">
+      <header
+        class="bg-zinc-100 h-10 flex items-center px-3 border-b border-zinc-300"
+      >
+        <h1 class="text-zinc-900 text-lg font-semibold">{{ title }}</h1>
+      </header>
+      <router-outlet></router-outlet>
+    </main>
+  `,
+  styles: `
+  `,
+})
+export class AppComponent {
+  title = 'Example app';
+}
