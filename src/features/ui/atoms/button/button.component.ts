8401e97f9102b90cb045719abcec0989116ac329
Mikhail Repin <migel72333@gmail.com>
2025-05-30 20:03:49 +0300

    Добавлены новые стили для кнопок в ButtonComponent: улучшены визуальные эффекты для состояний success, warning и danger, а также обновлены стили для ghost-кнопок. Изменены стили для состояния disabled, что повышает читаемость и функциональность компонентов.

--- src/features/ui/atoms/button/button.component.ts
+++ src/features/ui/atoms/button/button.component.ts
@@ -64,27 +64,80 @@ import { IconComponent } from '../icon/icon.component';
           }
         }
         &.secondary {
+          @apply text-text-default;
           &.fill {
-            @apply bg-secondary-default text-text-default;
+            @apply bg-secondary-default;
           }
           &.outline {
-            @apply border-elevation-border text-text-default;
+            @apply border-elevation-border;
             &:hover {
               @apply bg-secondary-default;
             }
           }
-          &.ghost {
-            @apply text-secondary-default;
+          &.ghost:hover {
+            @apply bg-secondary-default;
+          }
+        }
+        &.success {
+          &.fill {
+            @apply bg-success-default text-text-white;
+          }
+          &.outline {
+            @apply border-success-default text-success-default;
             &:hover {
-              @apply bg-secondary-default;
+              @apply bg-success-shaded-100;
+            }
+          }
+          &.ghost {
+            @apply text-success-default;
+            &:hover {
+              @apply bg-success-shaded-100;
+            }
+          }
+        }
+        &.warning {
+          &.fill {
+            @apply bg-warning-default text-text-white;
+          }
+          &.outline {
+            @apply border-warning-default text-warning-default;
+            &:hover {
+              @apply bg-warning-shaded-100;
+            }
+          }
+          &.ghost {
+            @apply text-warning-default;
+            &:hover {
+              @apply bg-warning-shaded-100;
+            }
+          }
+        }
+        &.danger {
+          &.fill {
+            @apply bg-danger-default text-text-white;
+          }
+          &.outline {
+            @apply border-danger-default text-danger-default;
+            &:hover {
+              @apply bg-danger-shaded-100;
+            }
+          }
+          &.ghost {
+            @apply text-danger-default;
+            &:hover {
+              @apply bg-danger-shaded-100;
             }
           }
         }
         &:hover {
           @apply filter brightness-90 cursor-pointer;
+          &.ghost,
+          &.outline {
+            @apply filter !brightness-100;
+          }
         }
         &.disabled {
-          @apply !bg-secondary-dark !text-text-disabled pointer-events-none;
+          @apply !bg-text-disabled !text-text-default pointer-events-none opacity-50;
         }
       }
     `,
